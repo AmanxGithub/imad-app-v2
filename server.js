ed5ea039e97a26f2483d188b246dbdcd4c40463a
AmanxGithub <vicky.amangupta@gmail.com>
2017-03-23 09:14:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.get('/article-one' , function (req, res) {
 });
 
 app.get('/article-two' , function (req, res) {
-    res.send('article two requested and will b served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three' , function (req, res) {
-    res.send('article three requested and will b served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
